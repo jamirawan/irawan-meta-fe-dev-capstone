5e6fee3db331489102d0c9b812db38472b819f93
Irawan <irawan@live.com.au>
2023-12-29 15:45:10 +0800

    added booking form page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   import Layout from './components/layout/Layout';
   import Home from './components/pages/Home';
   import About from './components/pages/About';
-  
+  import Bookings from './components/pages/Bookings';
   
   const App = () => {
     return (
@@ -14,9 +14,10 @@ import {
         <Layout>
           <Routes>
             <Route path={pages.get('home').path} element={<Home />} />
-           
-          <Route 
-            path={pages.get('about').path} element={<About/> } />
+           <Route 
+            path={pages.get('about').path} element={<About /> } />
+           <Route 
+            path={pages.get('bookings').path} element={<Bookings /> } />
           </Routes>
         </Layout>
       </>
